d25adf29c0bee87dfecfb4650a4474370a763b64
Nathan Betts <nathanbetts@hotmail.co.uk>
2020-11-06 19:45:31 +0000

    simplify App import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
-import App from 'App';
-import React from 'react';
+import firebase from 'utils/firebase';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
+import store from 'store';
+import Home from 'pages/Home';
+
+const App = () => {
+  const [appLoaded, setAppLoaded] = useState(false);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        store.update(s => {
+          s.email = user.email || '';
+        });
+      }
+      setAppLoaded(true);
+    });
+  }, []);
+
+  return appLoaded ? <Home /> : <p>Loading...</p>;
+};
 
 ReactDOM.render(
   <React.StrictMode>
