ec742ff0a62961f6358de56d3868735c58ecd2a5
Nathan Betts <nathanbetts@hotmail.co.uk>
2020-11-11 21:15:06 +0000

    update help info

--- src/components/HelpPopup/index.tsx
+++ src/components/HelpPopup/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Layer } from 'grommet';
+import { Box, Button, Heading, Layer, Paragraph } from 'grommet';
 import React from 'react';
 
 interface HelpPopupProps {
@@ -7,9 +7,13 @@ interface HelpPopupProps {
 
 const HelpPopup = ({ onClose }: HelpPopupProps) => {
   return (
-    <Layer full animation="fadeIn">
-      <Box fill background="light-4" align="center" justify="center">
-        <Button primary label="Close" onClick={onClose} />
+    <Layer onEsc={onClose} onClickOutside={onClose} animation="fadeIn">
+      <Box background="brand" align="center" justify="center" pad="medium" elevation="large">
+        <Heading margin={{ bottom: 'medium' }}>Help</Heading>
+        <Paragraph margin="xsmall">My Notes is a cloud app that allows you to saves notes in the cloud.</Paragraph>
+        <Paragraph margin="xsmall">The note editor can present notes in Markdown and are saved in real time.</Paragraph>
+        <Paragraph margin="xsmall">Notes can be deleted with the trash icon and will be immediately removed from the server.</Paragraph>
+        <Button primary label="Close" onClick={onClose} margin={{ top: 'medium' }} />
       </Box>
     </Layer>
   );
